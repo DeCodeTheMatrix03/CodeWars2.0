b5478381caeee43e142a27f9fd0296f3e3217a3b
Joel Nunez <90292480+DeCodeTheMatrix03@users.noreply.github.com>
2022-10-29 12:12:12 -0400

    Create Pointer.js

---
+++ solutions/Pointer.js
@@ -0,0 +1,12 @@
+/*
+Use a pointer to make two values equal to one another
+*/
+
+let obj1 = {
+value: 22
+}
+
+let obj2 = obj1
+
+
+//in this example obj2 will point to obj1 and update as obj1 changes
